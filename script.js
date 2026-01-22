4bb1b28a8ec27e527e4af8fd2a0bd1562fa0d0b8
xintongchen0804-creator <xintongchen0804@gmail.com>
2026-01-22 23:31:08 +0800

    Remove unused scrolling and highlighting functions
    Remove scrollToSection and toggleHighlight functions.

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
-    });
-}
 
-function toggleHighlight() {
-    const section = document.getElementById("activities");
-    section.classList.toggle("highlight");
-}
